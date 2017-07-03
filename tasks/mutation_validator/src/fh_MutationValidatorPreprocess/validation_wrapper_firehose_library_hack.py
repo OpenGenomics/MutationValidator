cd3640a42c33c95a9a6168ef705fb4a9ab722f3b
Gordon Saksena <gsaksena@broadinstitute.org>
2017-07-03 14:53:27 -0400

    tolerate empty gene names in maf

--- tasks/mutation_validator/src/fh_MutationValidatorPreprocess/validation_wrapper_firehose_library_hack.py
+++ tasks/mutation_validator/src/fh_MutationValidatorPreprocess/validation_wrapper_firehose_library_hack.py
@@ -47,8 +47,8 @@ def maf_to_position(filename): #convert maf to a position file with Chromosome, 
     with open(filename, 'rb') as csvfile:
         fullfile = csv.reader(csvfile, delimiter='\t')
         for line in fullfile:
-            print(line)
-            if len(line)>0 and line[0][0]!='#':
+            #print(line)
+            if len(line[0])>0 and line[0][0]!='#':
                 if count==0:
                     header=line
                     ind = [int(i) for i, x in enumerate(header) if x in columns_care]
